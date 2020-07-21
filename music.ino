622cff0cb13aecba53ff2f6e582b8bec77e64d76
Jiangtaodeng <66984711+Jiangtaodeng@users.noreply.github.com>
2020-07-21 23:32:38 +0800

    《她来听我的演唱会》

---
+++ music.ino
@@ -0,0 +1,210 @@
+#define NOTE_F0 -1 
+#define NOTE_F1 393 
+#define NOTE_F2 441 
+#define NOTE_F3 495 
+#define NOTE_F4 556 
+#define NOTE_F5 624 
+#define NOTE_F6 661 
+#define NOTE_F7 742 
+
+#define NOTE_FL1 196 
+#define NOTE_FL2 221 
+#define NOTE_FL3 234 
+#define NOTE_FL4 262 
+#define NOTE_FL5 294 
+#define NOTE_FL6 330 
+#define NOTE_FL7 371 
+
+#define NOTE_FH1 786 
+#define NOTE_FH2 882 
+#define NOTE_FH3 990 
+#define NOTE_FH4 1049 
+#define NOTE_FH5 1178
+#define NOTE_FH6 1322
+#define NOTE_FH7 1484 
+
+int tune[] =  
+{  
+NOTE_F0,NOTE_F3,NOTE_F3,NOTE_F3,NOTE_F3,NOTE_F3,NOTE_F3,NOTE_F3,NOTE_F1,
+NOTE_F1,NOTE_F0,NOTE_FL5,
+NOTE_F2,NOTE_F2,NOTE_F2,NOTE_F2,NOTE_F2,NOTE_F2,NOTE_F2,NOTE_F2,NOTE_F1,NOTE_F1,NOTE_FL7,NOTE_F1,NOTE_F2,
+NOTE_F2,NOTE_F2,NOTE_F2,NOTE_F2,
+NOTE_F0,NOTE_F3,NOTE_F5,NOTE_F5,NOTE_F3,NOTE_F5,NOTE_F5,NOTE_F5,NOTE_F6,NOTE_F5,NOTE_F6,
+NOTE_F6,NOTE_FH1,NOTE_FH1,NOTE_FH1,NOTE_F0,NOTE_FL6,NOTE_F1,
+NOTE_F3,NOTE_F2,NOTE_F2,NOTE_F2,NOTE_F2,NOTE_F0,NOTE_FL5,NOTE_FL5,NOTE_FL6,NOTE_F1,NOTE_FL6,NOTE_F1,
+NOTE_F1,NOTE_F1,NOTE_F1,NOTE_F3,NOTE_F5,NOTE_F5,
+NOTE_FH1,NOTE_FH1,NOTE_F3,NOTE_F5,NOTE_F5,NOTE_F0,NOTE_F3,NOTE_F5,
+NOTE_FH1,NOTE_FH1,NOTE_FH1,NOTE_F3,NOTE_F5,NOTE_F5,NOTE_F0,NOTE_F3,NOTE_F2,
+NOTE_F1,NOTE_F1,NOTE_F1,NOTE_F1,NOTE_FH1,NOTE_F7,NOTE_F6,NOTE_F6,NOTE_F5,NOTE_F3,NOTE_F3,
+NOTE_F2,NOTE_F2,NOTE_F2,NOTE_F0,NOTE_F3,
+NOTE_F2,NOTE_F3,NOTE_F2,NOTE_F3,NOTE_F2,NOTE_F3,NOTE_F3,NOTE_F2,NOTE_F3,NOTE_F5,NOTE_F5,NOTE_F3,
+NOTE_F2,NOTE_F3,NOTE_F2,NOTE_F3,NOTE_F2,NOTE_F3,NOTE_F2,NOTE_F2,NOTE_F3,NOTE_F5,NOTE_F5,
+NOTE_F0,NOTE_F6,NOTE_F5,NOTE_F6,NOTE_FH1,NOTE_F0,NOTE_F5,NOTE_F6,NOTE_F6,NOTE_FH1,NOTE_F3,
+NOTE_F3,NOTE_F2,NOTE_F1,NOTE_F1,
+NOTE_F0,
+NOTE_F0,NOTE_F6,NOTE_F5,NOTE_F6,NOTE_F1,NOTE_F0,NOTE_F5,NOTE_F6,NOTE_F6,NOTE_F1,NOTE_F3,
+NOTE_F3,NOTE_F2,NOTE_F1,NOTE_F1,NOTE_F0,NOTE_F5,
+NOTE_FH1,NOTE_FH7,NOTE_FH1,NOTE_FH3,NOTE_F7,NOTE_F6,NOTE_FH2,NOTE_F7,NOTE_F5,
+NOTE_F4,NOTE_F3,NOTE_F2,NOTE_F1,NOTE_F2,NOTE_F3,NOTE_F4,NOTE_F3,NOTE_F4,NOTE_F5,NOTE_F5,NOTE_FH1,NOTE_FH2,
+NOTE_FH2,NOTE_FH3,NOTE_FH2,NOTE_FH3,NOTE_FH3,NOTE_FH4,NOTE_FH3,NOTE_FH2,NOTE_FH1,NOTE_FH2,NOTE_FH3,NOTE_FH3,NOTE_FH1,
+NOTE_FH1,NOTE_FH1,NOTE_FH1,NOTE_F5,NOTE_F0,NOTE_F4,NOTE_F3,NOTE_F4,NOTE_F4,NOTE_F5,NOTE_F5,
+NOTE_F1,NOTE_F3,NOTE_F4,NOTE_F4,NOTE_F6,NOTE_F5,NOTE_F5,NOTE_F3,NOTE_F5,NOTE_F5,
+NOTE_FH1,NOTE_FH1,NOTE_F3,NOTE_F5,NOTE_F5,NOTE_F0,NOTE_F3,NOTE_F5,
+NOTE_FH1,NOTE_FH1,NOTE_FH1,NOTE_F3,NOTE_F5,NOTE_F5,NOTE_F0,NOTE_F3,NOTE_F2,
+NOTE_F1,NOTE_F1,NOTE_F1,NOTE_F1,NOTE_FH1,NOTE_F7,NOTE_F6,NOTE_F5,NOTE_F5,NOTE_F3,NOTE_F3,
+NOTE_F2,NOTE_F0,NOTE_F3,
+NOTE_F2,NOTE_F3,NOTE_F2,NOTE_F3,NOTE_F2,NOTE_F3,NOTE_F3,NOTE_F2,NOTE_F3,NOTE_F5,NOTE_F5,NOTE_F3,
+NOTE_F2,NOTE_F3,NOTE_F2,NOTE_F3,NOTE_F2,NOTE_F3,NOTE_F2,NOTE_F2,NOTE_F3,NOTE_F5,NOTE_F5,
+NOTE_F0,NOTE_F6,NOTE_F5,NOTE_F6,NOTE_FH1,NOTE_F0,NOTE_F5,NOTE_F6,NOTE_F6,NOTE_FH1,NOTE_F3,
+NOTE_F3,NOTE_F2,NOTE_F1,NOTE_F3,NOTE_F5,NOTE_F5,
+NOTE_F0,NOTE_F6,NOTE_F5,NOTE_F6,NOTE_FH1,NOTE_F0,NOTE_F5,NOTE_F6,NOTE_F6,NOTE_FH1,NOTE_F3,
+NOTE_F3,NOTE_F2,NOTE_F1,NOTE_F1,NOTE_F3,NOTE_F5,NOTE_F5,
+NOTE_F0,NOTE_F6,NOTE_F5,NOTE_F6,NOTE_FH1,NOTE_F0,NOTE_F5,NOTE_F6,NOTE_F6,NOTE_FH1,NOTE_F3,
+NOTE_F3,NOTE_F2,NOTE_F1,NOTE_F1,
+NOTE_F0,NOTE_F3,NOTE_F3,NOTE_F3,NOTE_F3,NOTE_F3,NOTE_F3,NOTE_F3,NOTE_F5,NOTE_F1,
+NOTE_F1,NOTE_F0,NOTE_F5,
+NOTE_F2,NOTE_F2,NOTE_F2,NOTE_F2,NOTE_F2,NOTE_F2,NOTE_F2,NOTE_F2,NOTE_F1,NOTE_FL7,NOTE_FL7,NOTE_F1,NOTE_F2,
+NOTE_F2,
+NOTE_F0,NOTE_F3,NOTE_F5,NOTE_F5,NOTE_F5,NOTE_F5,NOTE_F5,NOTE_F5,NOTE_F6,NOTE_F6,NOTE_F5,NOTE_F6,
+NOTE_F6,NOTE_F5,NOTE_F3,NOTE_F3,NOTE_F0,NOTE_F6,NOTE_F1,
+NOTE_F3,NOTE_F2,NOTE_F2,NOTE_F2,NOTE_F2,NOTE_F0,NOTE_FL5,NOTE_FL5,NOTE_FL6,NOTE_F1,NOTE_FL6,NOTE_F1,
+NOTE_F1,NOTE_F0,NOTE_FL6,NOTE_F1,
+NOTE_F3,NOTE_F2,NOTE_F2,NOTE_F2,NOTE_F2,NOTE_F0,NOTE_FL5,NOTE_FL5,NOTE_FL6,NOTE_F1,NOTE_F2,NOTE_F1,
+}
+;//曲子的音符部分
+
+float duration[]= 
+
+{  
+0.5,0.25,0.25,0.25,0.5,0.25+1,0.25,0.5,0.25,
+1+1+1,0.5+0.25,0.25,
+0.25,0.5,0.25,0.25,0.25,0.25,0.25,0.5+0.25,0.25,0.25,0.25,0.25,0.25,
+1,1,1,1,
+0.5,0.25,0.25,0.25,0.5,0.25,0.5+0.25,0.25,0.25,0.5,0.25,
+0.25,0.5+0.25,1,1,0.5,0.25,0.25,
+0.25,0.25,0.25,0.25,1,0.5,0.25,0.25,0.25,0.25,0.25,0.25,
+1,1,1,0.25,0.5,0.25,
+1,0.25,0.5,0.25,1,0.25,0.5,0.25,
+0.5,0.25,0.25,0.5,0.25,1,0.5,0.25,0.25,
+0.25,0.5,0.25,0.5+0.25,0.25,0.25,0.5,0.25,0.25,0.5,0.25,
+1,1,1,0.5+0.25,0.25,
+0.25,0.25,0.25,0.25,0.25,0.5,0.25,0.25,0.5,0.25,0.5+0.25,0.25,
+0.25,0.25,0.25,0.25,0.25,0.5,0.25,0.25,0.5,0.25,1,
+0.5,0.25,0.25,0.25,0.5+0.25,0.5,0.25,0.25,0.25,0.5,0.25,
+0.25,0.25,0.5,1+1+1,
+1+1+1+1,
+0.5,0.25,0.25,0.25,0.5+0.25,0.5,0.25,0.25,0.25,0.5,0.25,
+0.25,0.5,0.25,1+1,0.25,0.5+0.25,
+0.5,0.25,0.25,0.5,0.5,0.5,0.5,0.5,0.5,
+0.25,0.25,0.25,0.25,0.5,0.5,0.25,0.25,0.25,0.25,0.5,0.25,0.25,
+0.25,0.25,0.25,0.25,0.25,0.5,0.25,0.5,0.25,0.25,0.25,0.5,0.25,
+0.5+0.25,0.25,0.25,0.5+0.25,0.25,0.25,0.25,0.25,0.25,0.25,0.5,
+0.5,0.25,0.25,0.25,0.5,0.25,1,0.25,0.5,0.25,
+1,0.25,0.5,0.25,1,0.25,0.5,0.25,
+0.5+0.25,0.25,0.25,0.5,0.25,1,0.5,0.25,0.25,
+0.25,0.5,0.25,0.5+0.25,0.25,0.5,0.25,0.25,0.25,0.5,0.25,
+1+1+1,0.5+0.25,0.25,
+0.25,0.25,0.25,0.25,0.25,0.5,0.25,0.25,0.5,0.25,0.5+0.25,0.25,
+0.25,0.25,0.25,0.25,0.25,0.5,0.25,0.25,0.5,0.25,1,
+0.5,0.25,0.25,0.25,0.5+0.25,0.5,0.25,0.25,0.25,0.5,0.25,
+0.25,0.5,0.25,1+1,0.25,0.5,0.25,
+0.5,0.25,0.25,0.25,0.5+0.25,0.5,0.25,0.25,0.25,0.5,0.25,
+0.25,0.5,0.25,1+1+1,
+1,0.25,0.25,0.25,0.5,0.25,0.25,0.25,0.5,0.25,
+1+1+1,0.5+0.25,0.25,
+0.25,0.5,0.25,0.25,0.25,0.25,0.25,0.5+0.25,0.25,0.25,0.25,0.25,0.25,
+1+1+1+1,
+0.5,0.25,0.25,0.25,0.5,0.25,0.5+0.25,0.25,0.25,0.25,0.25,0.25,
+0.25,0.25,0.5,1+1,0.5,0.25,0.25,
+0.25,0.25,0.25,0.25,1,0.5,0.25,0.25,0.25,0.25,0.25,0.25,
+1+1+1,0.5,0.25,0.25,
+0.25,0.25,0.25,0.25,1,0.5,0.25,0.25,0.25,0.25,0.25,0.25,
+};//整首曲子的节拍部分
+
+int length;//音符个数
+int tonePin=5;//蜂鸣器pin 
+
+#include <ESP8266WiFi.h>
+#ifndef STASSID
+#define STASSID "HUAWEI nova 3"
+#define STAPSK  "66666666"
+#endif
+
+const char* ssid = STASSID;
+const char* password = STAPSK;
+
+WiFiServer server(80);
+
+void setup()
+{
+  length = sizeof(tune)/sizeof(tune[0]);
+  pinMode(tonePin, OUTPUT);
+  
+  Serial.begin(115200);
+  digitalWrite(tonePin, 0);
+
+  Serial.println();
+  Serial.println();
+  Serial.print(F("Connecting to "));
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(F("."));
+  }
+  
+  Serial.println();
+  Serial.println(F("WiFi connected"));
+
+  server.begin();
+  Serial.println(F("Server started"));
+  Serial.println(WiFi.localIP());
+} 
+
+void loop() 
+
+{ 
+  WiFiClient client = server.available();
+  Serial.println(F("new client"));
+  client.setTimeout(5000); // default is 1000
+  String req = client.readStringUntil('\r');
+  Serial.println(F("request: "));
+  Serial.println(req);
+  
+  int val;
+  if (req.indexOf(F("/gpio/0")) != -1) {
+    val = 0;
+    digitalWrite(tonePin, 0);
+  } else if (req.indexOf(F("/gpio/1")) != -1) {
+    val = 1;
+  } else {
+    Serial.println(F("invalid request"));
+    val = digitalRead(tonePin);  
+  }
+  
+  while (client.available()) {
+    client.read();
+  }
+
+  client.print(F("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\n music is now "));
+  client.print((val) ? F("on") : F("off"));
+  client.print(F("<br><br>Click <a href='http://"));
+  client.print(WiFi.localIP());
+  client.print(F("/gpio/1'>here</a> to turn on music, or <a href='http://"));
+  client.print(WiFi.localIP());
+  client.print(F("/gpio/0'>here</a> to turn off music.</html>"));
+  
+  Serial.println(F("Disconnecting from client"));
+ if(val==1)  { 
+  for(int x=0;x<length;x++)//循环音符的次数
+  { 
+ tone(tonePin,tune[x]);//此函数依次播放tune序列里的数组
+ delay(1000*duration[x]);
+ noTone(tonePin);
+  }
+  digitalWrite(tonePin, 1);
+ }
+}
